73a69de0353ffe8af24c81b9a59479e9271f4c3f
0xffsaultcoll3ge <19077385@saultcollege.ca>
2025-02-16 21:35:06 -0500

    Added /aAPI endpoints, database skeleton, etc.

--- api/app.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-from queue import Queue, PriorityQueue
-
-
-class APIService:
-    def __init__(self):
-        self.task_queue = PriorityQueue()
-        self.app = Flask(__name__)
-    s
